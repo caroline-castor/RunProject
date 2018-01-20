130a17457d0eca26bc9016fcbd40eb9e9e84593a
carolinecdsantos <carolinecdsantos@gmail.com>
2018-01-19 23:09:45 -0200

    first

---
+++ application/controllers/LogadoController.php
@@ -0,0 +1,21 @@
+<?php
+
+class LogadoController extends Zend_Controller_Action
+{
+
+    public function init()
+{
+    if ( !Zend_Auth::getInstance()->hasIdentity() ) {
+        return $this->_helper->redirector->goToRoute( array('controller' => 'auth'), null, true);
+    }
+}
+
+    public function indexAction()
+{
+    $usuario = Zend_Auth::getInstance()->getIdentity();
+    $this->view->usuario = $usuario;
+}
+
+
+}
+
